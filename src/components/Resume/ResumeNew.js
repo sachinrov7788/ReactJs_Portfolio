044d81d6a19c549140f3286f5d1e7eedb2381ac1
Sachin <100345202+sachin778899@users.noreply.github.com>
2023-01-25 11:49:27 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/SACHIN_RESUME.pdf.pdf";
+import pdf from "../../Assets/../Assets/SACHIN_RESUME.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/sachin778899/ReactJS_Portfolio/master/src/Assets/SACHIN_RESUME.pdf.pdf";
+const resumeLink = pdf;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
